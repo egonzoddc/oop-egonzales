1dbce037aa443557bc1ec46b56ed87cc2a6b2d28
Efren Gonzales <colorado3106@gmail.com>
2019-10-16 15:32:14 -0600

    setup for oop

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -1,1 +1,37 @@
 <?php
+
+/**
+ * Variables - authorId
+ * - authorEmail
+ * - authorHash
+ * - authorUsername
+ **/
+
+/**
+ * accessors for variables
+ */
+class Profile {
+
+	private $authorId;
+	private $authorEmail;
+	private $authorHash;
+	private $authorUsername;
+
+
+	public function getauthorId() {
+		return ($this->authorId);
+	}
+
+	public function getauthorEmail() {
+		return ($this ->authorEmail);
+	}
+
+	public function getauthorHash() {
+		return ($this ->authorHash);
+	}
+
+	public function getauthorUsername() {
+		return ($this ->authorUsername);
+	}
+}
+?>
--- sql/author.sql
+++ sql/author.sql
@@ -9,3 +9,4 @@ create table author(
 	unique(authorUsername),
 	primary key(authorId)
 );
+
