d55af88135639630881e5e3a3f9932339406ca95
Efren Gonzales <colorado3106@gmail.com>
2019-10-23 19:47:23 -0600

    OOP

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -1,3 +1,56 @@
 <?php
+/**
+ *
+ * insert statement method
+ * INSERT INTO Employee(ID, Name, Department, Salary) Values(1,'Anna','IT', 10000);
+ *
+ * update statement method
+ * UPDATE employees
+ * SET lastname = 'Smith'
+ * WHERE employeeid = 3;
+ *
+ * delete statement method
+ * DELETE FROM artists_backup
+ * WHERE artistid = 1;
+ *
+ * getFooByBar method that returns a single object
+ * class ObjectPassDemo
+ * {
+ * int a, b;
+ *
+ * ObjectPassDemo(int i, int j)
+ * {
+ * a = i;
+ * b = j;
+ * }
+ *
+ * // return true if o is equal to the invoking
+ * // object notice an object is passed as an
+ * // argument to method
+ * boolean equalTo(ObjectPassDemo o)
+ * {
+ * return (o.a == a && o.b == b);
+ * }
+ * }
+ * getFooByBar method that returns a full array
+ * {
+ *
+ * public static double min ( double a, double b )
+ * {
+ * double m = 0;
+ *
+ * if ( a < b )
+ * {
+ * m = a;    // a is the smaller value
+ * }
+ * else
+ * {
+ * m = b;    // b is the smaller value
+ * }
+ *
+ * return(m);   // Return value of the method
+ * }
+ * }
+ */
 
 ?>
